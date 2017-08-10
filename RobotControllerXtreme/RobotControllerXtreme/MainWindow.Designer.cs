5a60937e572d895e7e0da2b016a59a734e105a01
Matej Erceg <mat.erceg@gmail.com>
2017-08-10 12:33:28 +0200

    VS2015 aplikacija za upravljanje robotom

---
+++ RobotControllerXtreme/RobotControllerXtreme/MainWindow.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RobotControllerXtreme
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.ForwardButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SerialResponseBox = new System.Windows.Forms.TextBox();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ForwardButton
+            // 
+            this.ForwardButton.Location = new System.Drawing.Point(174, 27);
+            this.ForwardButton.Name = "ForwardButton";
+            this.ForwardButton.Size = new System.Drawing.Size(108, 65);
+            this.ForwardButton.TabIndex = 0;
+            this.ForwardButton.Text = "Forward";
+            this.ForwardButton.UseVisualStyleBackColor = true;
+            this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(174, 188);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(108, 66);
+            this.BackButton.TabIndex = 1;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(174, 111);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(108, 62);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // SerialResponseBox
+            // 
+            this.SerialResponseBox.Location = new System.Drawing.Point(12, 289);
+            this.SerialResponseBox.Name = "SerialResponseBox";
+            this.SerialResponseBox.Size = new System.Drawing.Size(497, 20);
+            this.SerialResponseBox.TabIndex = 3;
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Location = new System.Drawing.Point(43, 111);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(116, 62);
+            this.LeftButton.TabIndex = 4;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(302, 111);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(118, 62);
+            this.RightButton.TabIndex = 5;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 330);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.SerialResponseBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.ForwardButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "RobotControllerXtreme";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ForwardButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox SerialResponseBox;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button RightButton;
+    }
+}
+
---
+++ RobotControllerXtreme/RobotControllerXtreme/MainWindow.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace RobotControllerXtreme
+{
+    public partial class MainWindow : Form
+    {      
+        SerialPort port;                
+
+        public delegate void SerialPortStringConsumer(Control control, string text);
+
+        public MainWindow()
+        {           
+            InitializeComponent();
+            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.DataReceived += SerialDataHandler;       
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            SendCommand(new byte[] {0x00});
+        }
+
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            SendCommand(new byte[] {0x01});
+            //byte[] command = new byte[1];
+            //command[0] = 1;          
+            //port.Write(command,0,1);        
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            SendCommand(new byte[] {0x02});        
+        }       
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            SendCommand(new byte[] { 0x03 });
+            // SendCommand(3);
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            SendCommand(new byte[] { 0x04 });
+            //SendCommand(4);           
+        }
+
+        private void SendCommand(byte[] commands)
+        {
+            //byte[] commands = new byte[1];
+            //commands[0] = command;
+            port.Write(commands, 0, 1);
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+                SendCommand(new byte[] { 0x01 });
+            else if (e.KeyCode == Keys.Down)
+                SendCommand(new byte[] { 0x02 });
+            else if (e.KeyCode == Keys.Left)
+                SendCommand(new byte[] { 0x03 });
+            else if (e.KeyCode == Keys.Right)
+                SendCommand(new byte[] { 0x04 });
+            else
+                SendCommand(new byte[] { 0x00 });
+        }
+
+        private void SetSerialText(Control control, String text)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new SerialPortStringConsumer(SetSerialText), new object[] { control, text });  
+            }
+            else
+            {
+                control.Text = text;     
+            }            
+        }
+
+        private void SerialDataHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();        
+
+            SetSerialText(SerialResponseBox, indata);         
+            Console.Write(indata);
+        }
+
+       
+
+       
+    }
+}
