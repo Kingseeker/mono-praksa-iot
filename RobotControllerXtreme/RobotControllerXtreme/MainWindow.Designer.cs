af5fe93e2d287d5797b62d62a4052a30bd49d825
Matej Erceg <mat.erceg@gmail.com>
2017-08-16 12:34:26 +0200

    Desktop app update za Mqtt

--- RobotControllerXtreme/RobotControllerXtreme/MainWindow.Designer.cs
+++ RobotControllerXtreme/RobotControllerXtreme/MainWindow.Designer.cs
@@ -35,11 +35,18 @@
             this.SerialResponseBox = new System.Windows.Forms.TextBox();
             this.LeftButton = new System.Windows.Forms.Button();
             this.RightButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButon = new System.Windows.Forms.Button();
+            this.CommandsGroupBox = new System.Windows.Forms.GroupBox();
+            this.PublishButton = new System.Windows.Forms.Button();
+            this.ConnectionStatusBox = new System.Windows.Forms.ListBox();
+            this.PayloadTextBox = new System.Windows.Forms.TextBox();
+            this.CommandsGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // ForwardButton
             // 
-            this.ForwardButton.Location = new System.Drawing.Point(174, 27);
+            this.ForwardButton.Location = new System.Drawing.Point(128, 19);
             this.ForwardButton.Name = "ForwardButton";
             this.ForwardButton.Size = new System.Drawing.Size(108, 65);
             this.ForwardButton.TabIndex = 0;
@@ -49,7 +56,7 @@
             // 
             // BackButton
             // 
-            this.BackButton.Location = new System.Drawing.Point(174, 188);
+            this.BackButton.Location = new System.Drawing.Point(128, 158);
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(108, 66);
             this.BackButton.TabIndex = 1;
@@ -59,7 +66,7 @@
             // 
             // StopButton
             // 
-            this.StopButton.Location = new System.Drawing.Point(174, 111);
+            this.StopButton.Location = new System.Drawing.Point(128, 90);
             this.StopButton.Name = "StopButton";
             this.StopButton.Size = new System.Drawing.Size(108, 62);
             this.StopButton.TabIndex = 2;
@@ -69,14 +76,14 @@
             // 
             // SerialResponseBox
             // 
-            this.SerialResponseBox.Location = new System.Drawing.Point(12, 289);
+            this.SerialResponseBox.Location = new System.Drawing.Point(6, 246);
             this.SerialResponseBox.Name = "SerialResponseBox";
-            this.SerialResponseBox.Size = new System.Drawing.Size(497, 20);
+            this.SerialResponseBox.Size = new System.Drawing.Size(358, 20);
             this.SerialResponseBox.TabIndex = 3;
             // 
             // LeftButton
             // 
-            this.LeftButton.Location = new System.Drawing.Point(43, 111);
+            this.LeftButton.Location = new System.Drawing.Point(6, 90);
             this.LeftButton.Name = "LeftButton";
             this.LeftButton.Size = new System.Drawing.Size(116, 62);
             this.LeftButton.TabIndex = 4;
@@ -86,7 +93,7 @@
             // 
             // RightButton
             // 
-            this.RightButton.Location = new System.Drawing.Point(302, 111);
+            this.RightButton.Location = new System.Drawing.Point(242, 90);
             this.RightButton.Name = "RightButton";
             this.RightButton.Size = new System.Drawing.Size(118, 62);
             this.RightButton.TabIndex = 5;
@@ -94,21 +101,83 @@
             this.RightButton.UseVisualStyleBackColor = true;
             this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
             // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(39, 82);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 6;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DisconnectButon
+            // 
+            this.DisconnectButon.Location = new System.Drawing.Point(228, 82);
+            this.DisconnectButon.Name = "DisconnectButon";
+            this.DisconnectButon.Size = new System.Drawing.Size(75, 23);
+            this.DisconnectButon.TabIndex = 7;
+            this.DisconnectButon.Text = "Disconnect";
+            this.DisconnectButon.UseVisualStyleBackColor = true;
+            this.DisconnectButon.Click += new System.EventHandler(this.DisconnectButon_Click);
+            // 
+            // CommandsGroupBox
+            // 
+            this.CommandsGroupBox.Controls.Add(this.RightButton);
+            this.CommandsGroupBox.Controls.Add(this.LeftButton);
+            this.CommandsGroupBox.Controls.Add(this.StopButton);
+            this.CommandsGroupBox.Controls.Add(this.BackButton);
+            this.CommandsGroupBox.Controls.Add(this.ForwardButton);
+            this.CommandsGroupBox.Controls.Add(this.SerialResponseBox);
+            this.CommandsGroupBox.Location = new System.Drawing.Point(366, 21);
+            this.CommandsGroupBox.Name = "CommandsGroupBox";
+            this.CommandsGroupBox.Size = new System.Drawing.Size(370, 272);
+            this.CommandsGroupBox.TabIndex = 8;
+            this.CommandsGroupBox.TabStop = false;
+            this.CommandsGroupBox.Text = "Commands";
+            // 
+            // PublishButton
+            // 
+            this.PublishButton.Location = new System.Drawing.Point(132, 82);
+            this.PublishButton.Name = "PublishButton";
+            this.PublishButton.Size = new System.Drawing.Size(75, 23);
+            this.PublishButton.TabIndex = 9;
+            this.PublishButton.Text = "Publish";
+            this.PublishButton.UseVisualStyleBackColor = true;
+            this.PublishButton.Click += new System.EventHandler(this.PublishButton_Click);
+            // 
+            // ConnectionStatusBox
+            // 
+            this.ConnectionStatusBox.FormattingEnabled = true;
+            this.ConnectionStatusBox.Location = new System.Drawing.Point(83, 137);
+            this.ConnectionStatusBox.Name = "ConnectionStatusBox";
+            this.ConnectionStatusBox.Size = new System.Drawing.Size(168, 121);
+            this.ConnectionStatusBox.TabIndex = 10;
+            // 
+            // PayloadTextBox
+            // 
+            this.PayloadTextBox.Location = new System.Drawing.Point(39, 40);
+            this.PayloadTextBox.Name = "PayloadTextBox";
+            this.PayloadTextBox.Size = new System.Drawing.Size(264, 20);
+            this.PayloadTextBox.TabIndex = 11;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(521, 330);
-            this.Controls.Add(this.RightButton);
-            this.Controls.Add(this.LeftButton);
-            this.Controls.Add(this.SerialResponseBox);
-            this.Controls.Add(this.StopButton);
-            this.Controls.Add(this.BackButton);
-            this.Controls.Add(this.ForwardButton);
+            this.ClientSize = new System.Drawing.Size(761, 353);
+            this.Controls.Add(this.PayloadTextBox);
+            this.Controls.Add(this.ConnectionStatusBox);
+            this.Controls.Add(this.PublishButton);
+            this.Controls.Add(this.CommandsGroupBox);
+            this.Controls.Add(this.DisconnectButon);
+            this.Controls.Add(this.ConnectButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainWindow";
             this.Text = "RobotControllerXtreme";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
+            this.CommandsGroupBox.ResumeLayout(false);
+            this.CommandsGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +191,12 @@
         private System.Windows.Forms.TextBox SerialResponseBox;
         private System.Windows.Forms.Button LeftButton;
         private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButon;
+        private System.Windows.Forms.GroupBox CommandsGroupBox;
+        private System.Windows.Forms.Button PublishButton;
+        private System.Windows.Forms.ListBox ConnectionStatusBox;
+        private System.Windows.Forms.TextBox PayloadTextBox;
     }
 }
 
--- RobotControllerXtreme/RobotControllerXtreme/MainWindow.cs
+++ RobotControllerXtreme/RobotControllerXtreme/MainWindow.cs
@@ -1,31 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 using System.IO.Ports;
-using System.Threading;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace RobotControllerXtreme
 {
     public partial class MainWindow : Form
     {      
-        SerialPort port;                
+        SerialPort port;
+        MqttClient clientSub;
+        String connectionString = "192.168.0.102"; // CHANGE TO ACTUAL SERVER 
+        String hostName = "test.mosquitto.org"; // CHANGE TO ACTUAL SERVER
+        String topicName = "ArduinoControl"; // CHANGE TO ACTUAL TOPIC
 
-        public delegate void SerialPortStringConsumer(Control control, string text);
+        public delegate void SerialPortCallback(Control control, string text);
+        public delegate void MQTTCallback(Control control, string text);
 
         public MainWindow()
         {           
             InitializeComponent();
-            port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
-            port.Open();
-            port.DataReceived += SerialDataHandler;       
+
+            //port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            //port.Open();
+            //port.DataReceived += SerialDataHandler;
+
+            //HostIP = IPAddress.Parse(connectionString);
+            clientSub = new MqttClient(hostName);
+            //clientSub = new MqttClient(HostIP);
+            clientSub.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(EventPublished);
         }
+
+        #region  Command window
 
         private void StopButton_Click(object sender, EventArgs e)
         {
@@ -66,6 +73,7 @@ namespace RobotControllerXtreme
 
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
+            /*
             if (e.KeyCode == Keys.Up)
                 SendCommand(new byte[] { 0x01 });
             else if (e.KeyCode == Keys.Down)
@@ -76,13 +84,14 @@ namespace RobotControllerXtreme
                 SendCommand(new byte[] { 0x04 });
             else
                 SendCommand(new byte[] { 0x00 });
+            */
         }
 
         private void SetSerialText(Control control, String text)
         {
             if (control.InvokeRequired)
             {
-                control.Invoke(new SerialPortStringConsumer(SetSerialText), new object[] { control, text });  
+                control.Invoke(new SerialPortCallback(SetSerialText), new object[] { control, text });  
             }
             else
             {
@@ -99,8 +108,80 @@ namespace RobotControllerXtreme
             Console.Write(indata);
         }
 
+
+        #endregion
+
+
+        private void SetMQTTText(Control control, String text)
+        {            
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new MQTTCallback(SetMQTTText), new object[] { control, text});                
+            }
+            else
+            {
+                ((ListBox)control).Items.Add(text);
+            }
+        }
+
+        private void EventPublished(Object sender, MqttMsgPublishEventArgs e)
+        {
+            try
+            {
+                SetMQTTText(ConnectionStatusBox, "*** Received Message");
+                SetMQTTText(ConnectionStatusBox, "*** Topic: " + e.Topic);
+                SetMQTTText(ConnectionStatusBox, "*** Message: " + Encoding.UTF8.GetString(e.Message));
+                SetMQTTText(ConnectionStatusBox, "");
+            }
+            catch (InvalidCastException ex)
+            {
+            }
+        }          
+
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSub.Connect(hostName + "_sub");
+                SetMQTTText(ConnectionStatusBox, "* Client connected"); 
+                clientSub.Subscribe(new string[] { topicName }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                SetMQTTText(ConnectionStatusBox, "** Subscribing to: " + topicName);
+            }
+            catch (InvalidCastException ex)
+            {
+                MessageBox.Show("Failed to connect!");
+            }
+        }
+
+        private void DisconnectButon_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSub.Disconnect();
+                SetMQTTText(ConnectionStatusBox, "* Client disconnected");
+            }
+            catch (InvalidCastException ex)
+            {
+            }
+        }
+
+        private void PublishButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSub.Publish(topicName, Encoding.UTF8.GetBytes(PayloadTextBox.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                SetMQTTText(ConnectionStatusBox, "*** Publishing on: " + topicName);               
+            }
+            catch (InvalidCastException ex)
+            {
+            }
+        }
+
+     
+    }
+}
        
 
        
-    }
-}
+    
